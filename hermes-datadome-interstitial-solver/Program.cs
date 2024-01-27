cde0b2525a6f6091bc02a89e23f7ba82df0d4a46
ch-own <133884454+ch-own@users.noreply.github.com>
2024-01-27 16:03:18 +0100

    init

---
+++ hermes-datadome-interstitial-solver/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace hermes_datadome_interstitial_solver
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //from ddm object
+            string cid = "";
+            string hash = "";
+            string seed = "";
+
+            var encoder = new PayloadBuilder(cid, hash);
+            var t1 = double.Parse("5." + Random.Shared.Next(1111111111, int.MaxValue) + Random.Shared.Next(11111, 99999));
+            var t2 = double.Parse("3." + Random.Shared.Next(1111111111, int.MaxValue) + Random.Shared.Next(11111, 99999));
+            encoder.AddPair("tagpu", t1);
+            encoder.AddPair("tagpu", t2);
+            encoder.AddPair("plgod", false);
+            encoder.AddPair("plg", 5);
+            encoder.AddPair("plgne", true);
+            encoder.AddPair("plgre", true);
+            encoder.AddPair("plgof", false);
+            encoder.AddPair("plggt", false);
+            encoder.AddPair("pltod", false);
+            encoder.AddPair("psn", true);
+            encoder.AddPair("edp", false);
+            encoder.AddPair("addt", false);
+            encoder.AddPair("wsdc", true);
+            encoder.AddPair("ccsr", false);
+            encoder.AddPair("nuad", false);
+            encoder.AddPair("bcda", false);
+            encoder.AddPair("idn", true);
+            encoder.AddPair("capi", false);
+            encoder.AddPair("svde", false);
+            encoder.AddPair("vpbq", true);
+            encoder.AddPair("dvm", -1);
+            encoder.AddPair("vco", "");
+            encoder.AddPair("vco", "NA");
+            encoder.AddPair("vch", "NA");
+            encoder.AddPair("vcw", "NA");
+            encoder.AddPair("vc3", "NA");
+            encoder.AddPair("vcmp", "NA");
+            encoder.AddPair("vcq", "NA");
+            encoder.AddPair("vc1", "NA");
+            encoder.AddPair("vcots", "NA");
+            encoder.AddPair("vchts", "NA");
+            encoder.AddPair("vcwts", "NA");
+            encoder.AddPair("vc3ts", "NA");
+            encoder.AddPair("vcmpts", "NA");
+            encoder.AddPair("vcqts", "NA");
+            encoder.AddPair("vc1ts", "NA");
+            encoder.AddPair("aco", "");
+            encoder.AddPair("aco", "NA");
+            encoder.AddPair("acmp", "NA");
+            encoder.AddPair("acw", "NA");
+            encoder.AddPair("acma", "NA");
+            encoder.AddPair("acaa", "NA");
+            encoder.AddPair("ac3", "NA");
+            encoder.AddPair("acf", "NA");
+            encoder.AddPair("acmp4", "NA");
+            encoder.AddPair("acmp3", "NA");
+            encoder.AddPair("acwm", "NA");
+            encoder.AddPair("acots", "NA");
+            encoder.AddPair("acmpts", "NA");
+            encoder.AddPair("acwts", "NA");
+            encoder.AddPair("acmats", "NA");
+            encoder.AddPair("acaats", "NA");
+            encoder.AddPair("ac3ts", "NA");
+            encoder.AddPair("acfts", "NA");
+            encoder.AddPair("acmp4ts", "NA");
+            encoder.AddPair("acmp3ts", "NA");
+            encoder.AddPair("acwmts", "NA");
+            encoder.AddPair("lg", "fr-FR");
+            encoder.AddPair("npmtm", "NA");
+            encoder.AddPair("phe", false);
+            encoder.AddPair("nm", false);
+            encoder.AddPair("awe", false);
+            encoder.AddPair("geb", false);
+            encoder.AddPair("dat", false);
+            encoder.AddPair("sqt", false);
+            encoder.AddPair("ucdv", false);
+            encoder.AddPair("tzp", "Europe/Paris");
+            encoder.AddPair("tz", -120);
+            encoder.AddPair("rs_w", 390);
+            encoder.AddPair("rs_h", 844);
+            encoder.AddPair("isb", false);
+            encoder.AddPair("plu", "PDF Viewer,Chrome PDF Viewer,Chromium PDF Viewer,Microsoft Edge PDF Viewer,WebKit built-in PDF");
+            encoder.AddPair("mmt", "application/pdf,text/pdf");
+            encoder.AddPair("hcovdr", false);
+            encoder.AddPair("plovdr", false);
+            encoder.AddPair("ftsovdr", false);
+            encoder.AddPair("hcovdr2", false);
+            encoder.AddPair("plovdr2", false);
+            encoder.AddPair("ftsovdr2", false);
+            encoder.AddPair("glvd", "Apple Inc.");
+            encoder.AddPair("glrd", "Apple GPU");
+            encoder.AddPair("hc", 4);
+            encoder.AddPair("br_oh", 844);
+            encoder.AddPair("br_ow", 390);
+            encoder.AddPair("ua", "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Mobile/15E148 Safari/604.1");
+            encoder.AddPair("wbd", false);
+            encoder.AddPair("ts_mtp", 5);
+            encoder.AddPair("wwl", false);
+            encoder.AddPair("response", PayloadBuilder.EncodeSeed(seed));
+
+            var payload = encoder.Build();
+            Console.WriteLine(payload);
+        }
+    }
+}
---
+++ hermes-datadome-interstitial-solver/PropertyEncoder.cs
@@ -0,0 +1,159 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace hermes_datadome_interstitial_solver
+{
+    internal class PayloadBuilder
+    {
+        private const long _X = 4046101435;
+        private const string _r3n = "r3n";
+
+        private long _date;
+        private long _date2;
+        private int _k;
+
+        private readonly List<int> _kBytes = new();
+        private readonly List<int[]> _keyBytes = new();
+        private readonly List<int[]> _valueBytes = new();
+
+        private string _cid;
+        private string _hash;
+        private int _seed;
+        private int _r1;
+        private int _r2;
+        private int _F;
+        private int _u;
+
+        public PayloadBuilder(string cid, string hash)
+        {
+            _cid = cid;
+            _hash = hash;
+            _u = -1;
+            _r1 = Random.Shared.Next(100, 1000);
+            _r2 = Random.Shared.Next(100, 1000);
+            _date = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            _date2 = _date + (_r1 + _r2 * 2);
+            _k = (int)(255 & _date);
+            _seed = GetPayloadEncoderSeed();
+            _F = (int)(_seed ^ _date2);
+        }
+
+        public string Build()
+        {
+            var r = new List<int>();
+            for (int i = 0; i < _keyBytes.Count; i++)
+            {
+                var B = i == 0 ? 123 : 44;
+                r.Add(B ^ _kBytes[2 * i]);
+                r.AddRange(_keyBytes[i]);
+                r.Add(58 ^ _kBytes[(2 * i) + 1]);
+                r.AddRange(_valueBytes[i]);
+            }
+
+            var e = new List<int>();
+            e.Add(44 ^ GetByte());
+            e.AddRange(EncodeString(_r3n));
+            e.Add(58 ^ GetByte());
+            e.AddRange(EncodeString(255 & _seed));
+            r.AddRange(e);
+            r.Add(125 ^ GetByte());
+            var L = new List<char>();
+            for (int i = 0; i < r.Count;)
+            {
+                var R = (SafeXor(r, i++, _k) << 16) | (SafeXor(r, i++, _k) << 8) | SafeXor(r, i++, _k);
+                L.Add((char)M((R >> 18) & 63));
+                L.Add((char)M((R >> 12) & 63));
+                L.Add((char)M((R >> 6) & 63));
+                L.Add((char)M(63 & R));
+            }
+            var V = r.Count % 3;
+            if (V != 0)
+            {
+                for (int i = 0; i < 3 - V; i++)
+                    L.RemoveAt(L.Count - 1);
+            }
+            return string.Concat(L);
+        }
+
+        private static int SafeXor(List<int> l, int index, int kval)
+        {
+            if (index >= l.Count)
+                return kval;
+            return l[index] ^ kval;
+        }
+
+        private static int M(int j)
+        {
+            if (j > 37)
+                return 59 + j;
+            else if (j > 11)
+                return 53 + j;
+            else if (j > 1)
+                return 46 + j;
+            else
+                return (50 * j) + 45;
+        }
+
+        public void AddPair(string key, object value)
+        {
+            _kBytes.Add(GetByte());
+            _keyBytes.Add(EncodeString(key));
+            _kBytes.Add(GetByte());
+            _valueBytes.Add(EncodeString(value));
+        }
+
+        private int[] EncodeString(object str)
+        {
+            var s = JsonSerializer.Serialize(str);
+            var e = Encoding.UTF8.GetBytes(s);
+            var r = new int[e.Length];
+            for (int i = 0; i < e.Length; i++)
+                r[i] = e[i] ^ GetByte();
+            return r;
+        }
+
+        private int GetByte()
+        {
+            unchecked
+            {
+                if (++_u > 2)
+                {
+                    _F ^= _F << 13;
+                    _F ^= _F >> 17;
+                    _F ^= _F << 5;
+                    _u = 0;
+                }
+                var K = 16 - (8 * _u);
+                int j = 0;
+                for (int g = 3; g >= 0; g--)
+                    j |= _k << (8 * g);
+                return ((j ^ _F) >> K) & 255;
+            }
+        }
+
+        private int GetPayloadEncoderSeed()
+        {
+            unchecked
+            {
+                int c = D(_cid) ^ (int)(_date + _r1 + 2 * _r2) ^ D(_hash);
+                return (int)(c ^ _X);
+            }
+        }
+
+        private static int D(string s)
+        {
+            int S = 0;
+            for (int F = 0; F < s.Length; F++)
+                S = ((S << 5) - S + s[F]) << 0;
+            return S == 0 ? 1789537805 : S;
+        }
+
+        public static int EncodeSeed(string ddmSeed)
+        {
+            int r = 0;
+            for (int i = 0; i < ddmSeed.Length; ++i)
+                r += ddmSeed[i % 240];
+            return r;
+        }
+    }
+}
